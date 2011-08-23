6d4c92a0def3282d71cbdc9f5bc70f1112bbfb60
Justin <justin@justin-laptop.(none)>
2011-08-23 16:33:45 -0400

    Initial file

---
+++ xmonad.hs
@@ -0,0 +1,8 @@
+-- My xmonad settings
+-- So far only used on Fedora 15, YMMV
+import XMonad
+
+main = xmonad defaultConfig
+        { terminal = "gnome-terminal"
+          -- more changes
+        }
