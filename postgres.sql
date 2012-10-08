75b2a96ded71253e1c179cf981d67b7686ca265c
Justin Ethier <justin.ethier@gmail.com>
2012-10-08 14:49:23 -0300

    Update postgres.sql

--- postgres.sql
+++ postgres.sql
@@ -4,3 +4,5 @@ SELECT pg_stat_get_backend_pid(s.backendid) AS procpid,
        pg_stat_get_backend_client_addr(s.backendid) as ip
   FROM (SELECT pg_stat_get_backend_idset() AS backendid) AS s;
 
+-- Number of active connections
+SELECT sum(numbackends) FROM pg_stat_database;
