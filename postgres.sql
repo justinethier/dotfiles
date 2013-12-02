9d6f3886da60bcdcf6c9f2dcaaff5a6af65e6f4e
Justin Ethier <justin.ethier@gmail.com>
2013-12-02 11:12:46 -0500

    Update postgres.sql

--- postgres.sql
+++ postgres.sql
@@ -6,3 +6,16 @@ SELECT pg_stat_get_backend_pid(s.backendid) AS procpid,
 
 -- Number of active connections
 SELECT sum(numbackends) FROM pg_stat_database;
+
+-- Primary keys by table
+SELECT
+  tc.constraint_name, tc.table_name, kcu.column_name
+--    , ccu.table_name AS foreign_table_name,    ccu.column_name AS foreign_column_name
+FROM information_schema.table_constraints AS tc
+JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name
+-- JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name
+   WHERE constraint_type = 'PRIMARY KEY' -- AND tc.table_name='mytable';
+ORDER BY tc.table_name, kcu.column_name
+   
+-- Working with ctid (unique ID for each DB row)
+SELECT MAX(ctid) FROM my_table
