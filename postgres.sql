0a1a3e895f7987841ea545b68b96d2f3dd185a17
Justin Ethier <justin.ethier@gmail.com>
2019-01-30 11:12:33 -0500

    Update postgres.sql

--- postgres.sql
+++ postgres.sql
@@ -1,3 +1,15 @@
+-- Largest N tables
+select schemaname as table_schema,
+    relname as table_name,
+    pg_size_pretty(pg_total_relation_size(relid)) as total_size,
+    pg_size_pretty(pg_relation_size(relid)) as data_size,
+    pg_size_pretty(pg_total_relation_size(relid) - pg_relation_size(relid))
+      as external_size
+from pg_catalog.pg_statio_user_tables
+order by pg_total_relation_size(relid) desc,
+         pg_relation_size(relid) desc
+limit 10;
+                                                                    
 -- get list of currently running postgres queries
 SELECT pg_stat_get_backend_pid(s.backendid) AS procpid,
        pg_stat_get_backend_activity(s.backendid) AS current_query, 
