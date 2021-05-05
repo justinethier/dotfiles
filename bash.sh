8a42e51f8034f8d29591cdc124f908596a33ab41
Justin Ethier <justin.ethier@gmail.com>
2021-05-05 09:59:56 -0400

    Added snippet to find largest files

--- bash.sh
+++ bash.sh
@@ -2,6 +2,10 @@
 ### Useful bash commands
 ###
 
+# Find largest files on the system
+du -a /dir/ | sort -n -r | head -n 20
+
 # Delete all matching files. In this case, delete old CVS files
 find . -name ".#*" -delete
+
 
