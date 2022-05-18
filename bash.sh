99d4b2f8a2db144523b6e75726d79894b33183ad
Justin Ethier <justin.ethier@gmail.com>
2022-05-18 10:40:53 -0400

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -8,4 +8,5 @@ du -a /dir/ | sort -n -r | head -n 20
 # Delete all matching files. In this case, delete old CVS files
 find . -name ".#*" -delete
 
-
+# Count by hour. Can modify to get more resolution'
+grep "search string" syslog | egrep "April .. .." -o | sort | uniq -c
